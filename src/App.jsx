56ef3319f1d52492fdd09bad8cfba01d8cb9b258
Alexxe <Alexxe@fi.unju.edu.ar>
2023-10-30 18:02:12 -0300

    commit-2:descargar dependencias three js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Canvas } from "@react-three/fiber"
 import { Sky } from "@react-three/drei"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+function App() { 
+ return ( 
     <div>
-      <h1>proyecto</h1>
+      <Canvas>
+        <Sky />
+      </Canvas>
     </div>
   )
 }

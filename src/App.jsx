d826b84464c0f8809028643aa68139c5690f8641
Alexxe <Alexxe@fi.unju.edu.ar>
2023-10-30 17:50:30 -0300

    commit-1:crear app y configuar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Canvas } from "@react-three/fiber"
+import { Sky } from "@react-three/drei"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>proyecto</h1>
+    </div>
+  )
+}
+
+export default App
